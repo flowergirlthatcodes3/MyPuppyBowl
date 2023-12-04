335bf36d1d58f8402b71fe94bf55bd0a44669fa2
flowergirlthatcodes3 <149510282+flowergirlthatcodes3@users.noreply.github.com>
2023-12-03 23:33:32 -0500

    editing

--- puppy.js
+++ puppy.js
@@ -1,32 +1,17 @@
 // declaring all variables
 
-const allPupDiv = document.querySelector(".allPup")
-const singlePupDiv = document.querySelector(".singlePup")
+const allPupDiv = document.querySelector("#allPupDiv")
+const singlePupDiv = document.querySelector("#singlePupDiv")
 let puppies = []
 
+
+
+
 window.addEventListener("hashchange", () => {
-renderPups()
-})
+    renderPups()
+    })
 
-async function renderPups() {
-    const pupPlayer = puppies.map((pups) =>{
-      return `
-      <div class="all">
-      <a href=#${pups.name} class="pupName"> 
-      <h2> Puppy Name: <br> ${pups.name} </h2> 
-      </a>
-      <a href=#${pups.breed} class="pupBreed"> 
-      <h2> Puppy Breed: <br> ${pups.breed} </h2>
-      </a>
-      <a href=#${pups.status} class="pupStatus"> 
-      <h2> Puppy Status: <br> ${pups.status} </h2>
-      </a>
-  </div>
-      `
-      })
 
-      allPupDiv.innerHTML = pupPlayer.join("")
-  }
 
 async function fetchAllPuppies() {
     const response = await fetch("https://fsa-puppy-bowl.herokuapp.com/api/2310/players")
@@ -38,5 +23,56 @@ async function fetchAllPuppies() {
 
 
 
+
+async function renderPups() {
+    const pupPlayer = puppies.map((pups) =>{
+      return `
+      <div class="all">
+      <a href=#${pups.name} class="pupName"> 
+      ${pups.name}
+      </a>
+      <br> 
+  </div>
+      `
+      })
+      allPupDiv.innerHTML = "<h1> All Players! </h1>" + pupPlayer.join("")
+      const name = window.location.hash.slice(1)
+      const SinglePup = puppies.find((pups) => {
+          return pups.name === name 
+      
+      })
+   //console.log(SinglePup)
+allPupDiv.innerHTML = SinglePup ? "" : "<h1> All Players! </h1>" + `<div class="pupContainer"> ${pupPlayer.join("")} </div>` 
+   
+
+if(SinglePup) {
+    
+        singlePupDiv.innerHTML = `
+        <div id="pupStats">
+        <h2> Look Who's On The Field <br> ${SinglePup.name}</h2>
+        <br>
+        <h3> Breed: ${SinglePup.breed} </h3>
+        <br>
+        <h3> Status: ${SinglePup.status} </h3>
+        </div>
+        <div id ="pupImg">
+        <img src=${SinglePup.imageUrl}>
+        </div>
+        </div>` 
+       // + pupPlayer.join("")
+
+         + `<a href=#> Let's Get Back To All Players </a>`
+
+    }
+    else{
+        singlePupDiv.innerHTML = ""
+    } 
+
+
+      
+        
+
+
+}
 
 fetchAllPuppies()
